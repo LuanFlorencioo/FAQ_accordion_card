44d2da17812fefa61b0f1772a0620f1b69fb78b7
LuanFlorencioo <luanflorencio87@gmail.com>
2022-02-03 20:03:24 -0300

    script

---
+++ js/accordion.js
@@ -0,0 +1,51 @@
+const allQuestions = Array.from(document.querySelectorAll('.question'))
+const allAnswer = Array.from(document.querySelectorAll('.answer'));
+const allArrow = Array.from(document.querySelectorAll('img.arrow'))
+
+function handleClick(e) {
+    const localClick = e.target.localName;
+
+    if (localClick === 'dt') {
+        const question = e.target;
+        const answer = e.target.nextElementSibling;
+        const arrow = e.target.lastChild;
+
+        if (question.classList.contains('active')) {
+
+            [question, answer, arrow].forEach(item => item.classList.toggle('active'));
+
+        } else {
+
+            allQuestions.forEach(question => question.classList.remove('active'));
+            allAnswer.forEach(answer => answer.classList.remove('active'));
+            allArrow.forEach(arrow => arrow.classList.remove('active'));
+
+            [question, answer, arrow].forEach(item => item.classList.toggle('active'));
+
+        }
+
+    } else {
+        const arrow = e.target;
+        const question = e.path[1];
+        const answer = e.path[1].nextElementSibling;
+
+        if (arrow.classList.contains('active')) {
+
+            [question, answer, arrow].forEach(item => item.classList.toggle('active'));
+
+        } else {
+
+            allQuestions.forEach(question => question.classList.remove('active'));
+            allAnswer.forEach(answer => answer.classList.remove('active'));
+            allArrow.forEach(arrow => arrow.classList.remove('active'));
+
+            [question, answer, arrow].forEach(item => item.classList.toggle('active'));
+
+        }
+        document.querySelector('h1').classList.toggle('active')
+    }
+}
+
+allQuestions.forEach((item) => {
+    item.addEventListener('click', handleClick)
+})
